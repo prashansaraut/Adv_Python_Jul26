0db560e11efbac7504e7187e737a6fbe0586b93c
Prashansa Raut <rautprashansa91@gmail.com>
2026-07-10 17:20:02 +0530

    Added Python programs

---
+++ Day 3/P7.py
@@ -0,0 +1,23 @@
+
+import datetime
+class SimpleMeta():
+    def __new__(cls,name,bases,attrs):
+        print(f"Creating classes: {name}")
+        print(f"Base classes: {bases}")
+        print(f"Attributes:{list(attrs.keys())}")
+
+        attrs['created_at'] = datetime.datetime.now()
+        attrs['created_by'] = 'SimpleMeta'
+
+        return super().__new__(cls,name,bases,attrs)
+    
+class MyClass(metaClass=SimpleMeta):
+    x = 10
+
+    def method(self):
+        return "Hello"
+    
+print(MyClass.x)
+print(MyClass.created_at)
+print(MyClass.created_by)
+
