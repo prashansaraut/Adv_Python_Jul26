ae24a3a3718927e4c5f334f0918204d82f71d434
Prashansa Raut <rautprashansa91@gmail.com>
2026-07-10 22:29:23 +0530

    Higher Order Functions

---
+++ Day 4/P11.py
@@ -0,0 +1,18 @@
+
+# Higher Order Functions
+
+def apply_twice(func,value):
+    return func(func(value))
+
+def add_two(x):
+    return x+2
+
+print(apply_twice(add_two,10))
+
+def make_logger(prefix):
+    def logger(message):
+        print(f"[{prefix}] {message}")
+    return logger
+
+info_logger = make_logger("INFO")
+info_logger("System Started")
